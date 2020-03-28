d03c029f5606416ab554ab27dcde92115ccc2861
John Kueh <yoongfook@gmail.com>
2020-03-29 10:10:44 +1100

    Update firebase config

--- packages/react-firebase-example/src/lib/firebase.ts
+++ packages/react-firebase-example/src/lib/firebase.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBT9QlTM9kC_nCTmqZ10ZQKUI1A3TGIkHI",
-  authDomain: "react-firebase-example-e70ad.firebaseapp.com",
-  databaseURL: "https://react-firebase-example-e70ad.firebaseio.com",
-  projectId: "react-firebase-example-e70ad",
-  storageBucket: "react-firebase-example-e70ad.appspot.com",
-  messagingSenderId: "388501788591",
-  appId: "1:388501788591:web:615b6933ed0a42d687415a",
-  measurementId: "G-9YP9GSWW6E"
+  apiKey: "AIzaSyBL6FLCF0MAAOkAI5RI8OrtO3ACgk02RBo",
+  authDomain: "react-firebase-example-cb711.firebaseapp.com",
+  databaseURL: "https://react-firebase-example-cb711.firebaseio.com",
+  projectId: "react-firebase-example-cb711",
+  storageBucket: "react-firebase-example-cb711.appspot.com",
+  messagingSenderId: "501645646841",
+  appId: "1:501645646841:web:059947eb5e92efe274bb28",
+  measurementId: "G-15MPZ83G7S"
 };
